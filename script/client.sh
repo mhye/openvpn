ef1533d351cfd8d05679fa36ea2a937aa43c75e3
mhye <newguyer@qq.com>
2017-04-11 12:46:09 +0800

    Update client.sh

--- script/client.sh
+++ script/client.sh
@@ -5,15 +5,15 @@
 #route-up client.sh
 #route-pre-down
 #and make sure you have "script-security 2" in the config file
-source="0.0.0.0/0" #locat network area,such as ：192.168.1.0/24
+source_ip=`ip route show dev eth0|awk 'NR==2 {print $1}'` #if your local network interface is not eth0,you dead.
 if [ "$script_type" = "route-up" ];
 then
         ip=$ifconfig_local
-        /sbin/iptables -t nat -A POSTROUTING -s $source -o tun0 -j SNAT --to-source $ip
+        /sbin/iptables -t nat -A POSTROUTING -s $source_ip -o tun0 -j SNAT --to-source $ip
 elif [ "$script_type" = "route-pre-down" ];
 then
         ip=$ifconfig_local
-        /sbin/iptables -t nat -D POSTROUTING -s $source -o tun0 -j SNAT --to-source $ip
+        /sbin/iptables -t nat -D POSTROUTING -s $source_ip -o tun0 -j SNAT --to-source $ip
 
 fi
 
